af7354e593ecd538d39c6d429c0d0cc96d4c7306
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-06 20:01:48 +0100

    Update Factorialize a Number.js

--- Factorialize a Number.js
+++ Factorialize a Number.js
@@ -1,11 +1,9 @@
 function factorialize(num) {
- if (num == 0) {
+ if (num == 0) {  // terminal condition (to check the end of the recursion)
    return 1;
  }else{
-   return num * factorialize(num -1);
+   return num * factorialize(num -1);  // the function keeps summing up num * (num - 1) until num is 0. In this Case 5 * (5-1) * (4-1)...
  }
-
- return num;
 }
 
 factorialize(5);
