c407f2906d8548f6b25a7529a7db5264ee98d92e
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-07 17:26:40 +0100

    Update Find longest Word in String.js

--- Find longest Word in String.js
+++ Find longest Word in String.js
@@ -1,16 +1,15 @@
 function findLongestWord(str) {
-  var splitStr = str.split(" ");
+  var splitStr = str.split(" ");              // Splits String into array 
 
-  var lngth = 0;
-  var longestStr;
+  var lngth = 0;                              // lngth will hold the length of the longest word
 
-  for (i = 0; i < splitStr.length; i++) {
-    if (splitStr[i].length > lngth) {
-      lngth = splitStr[i].length;
+  for (i = 0; i < splitStr.length; i++) {     // loops through the array 
+    if (splitStr[i].length > lngth) {        
+      lngth = splitStr[i].length;             // sets lngth equal to  the number of letters in the longest word
 
     }
   }
     return lngth;
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("The quick brown fox jumped over the lazy dog"); // in this case 6
