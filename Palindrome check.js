089b183acd541f866149d1ab379bbd2608135db8
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-07 17:14:13 +0100

    Update Palindrome check.js

--- Palindrome check.js
+++ Palindrome check.js
@@ -1,9 +1,9 @@
 function palindrome(str) {
-  var re = /[\W_]/g;
-  var lowRegStr = str.toLowerCase().replace(re, '');
-  var reverseStr = lowRegStr.split('').reverse().join('');
+  var re = /[\W_]/g;                                          // global search for all non-word characters in a string  
+  var lowRegStr = str.toLowerCase().replace(re, '');          // set all letters to lower case and replace non word characters with ''
+  var reverseStr = lowRegStr.split('').reverse().join('');    // Reverse the String
 
-  if (reverseStr === lowRegStr) {
+  if (reverseStr === lowRegStr) {                             // if reversed string and string are the same return true, else false
     return true;
   }else {
     return false;
