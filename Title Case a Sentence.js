6d03b73f58cfae0e05a9a61ee57dcc86dc38dd88
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-08 17:01:07 +0100

    Update Title Case a Sentence.js

--- Title Case a Sentence.js
+++ Title Case a Sentence.js
@@ -1,7 +1,9 @@
 function titleCase(str) {
-  var convertToArray = str.toLowerCase().split(" ");
-  var result = convertToArray.map(function(val) {
-    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  var convertToArray = str.toLowerCase().split(" ");                    // all letters lowercase + split string into array at " "
+  var result = convertToArray.map(function(val) {                       // enter every word in the array and replace the first character
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());     // with its capital version
   });
-  return result.join(" ");
+  return result.join(" ");                                              // convert the array into a string again
 }
+
+titleCase("I'm a little tea pot");
