da3aa25b2cbe7842a71c4afc81602f440cf6ca5c
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-09 21:44:11 +0100

    Update Check for Mutations.js

--- Check for Mutations.js
+++ Check for Mutations.js
@@ -1,9 +1,10 @@
-function mutation(arr) {
-  var lower1 = arr[1].toLowerCase();
+function mutation(arr) {      
+  var lower1 = arr[1].toLowerCase();      // convert both words in the array to lowercase
   var lower0 = arr[0].toLowerCase();
 
-  for(i = 0; i < lower1.length; i++) {
-    if(lower0.indexOf(lower1[i]) == -1)
+  for(i = 0; i < lower1.length; i++){     // loops through every character of the second word in the array
+    if(lower0.indexOf(lower1[i]) == -1)   // if the character is not in the first word of the array return false
       return false;
-  }return true;
+  }return true;                           // when every character of the second word is in the first too, return true
 }
+mutation(["hello", "hey"])
