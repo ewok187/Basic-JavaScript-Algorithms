05fdf2c2139ff9da1f890c60267aaa5a301f184c
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-10 22:46:11 +0100

    Update Caesars Cipher.js

--- Caesars Cipher.js
+++ Caesars Cipher.js
@@ -1,14 +1,14 @@
 function caesar(str) {
-  return str.split("").map.call(str, function(char) {
-    var x = char.charCodeAt(0);
-    if (x < 65 || x > 90) {
-       return String.fromCharCode(x);
-    } else if (x < 78) {
-      return String.fromCharCode(x + 13);
-    } else {
-      return String.fromCharCode(x - 13);
+  return str.split("").map.call(str, function(char) { // splits the string in an array of the individual characters, processes every entry
+    var x = char.charCodeAt(0);                       // x holds the charCode of the different characters
+    if (x < 65 || x > 90) {                           // if its not in the alphabet just return the string 
+       return String.fromCharCode(x);                 
+    } else if (x < 78) {                              // if its between the first and 13th alphabet letter add 13 to char code 
+      return String.fromCharCode(x + 13);             // and return the string
+    } else {                                          // if its between the 13th and 26th letter then substrate 13 from char code
+      return String.fromCharCode(x - 13);             // and return the string
     }
-  }).join("");
+  }).join("");                                        // array -> string 
 }
 
 caesar("SERR PBQR PNZC");
