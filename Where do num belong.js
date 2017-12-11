c57dddf6b9e1ef523efda4b7378109e8a3d10a7e
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-11 18:13:56 +0100

    Update Where do num belong.js

--- Where do num belong.js
+++ Where do num belong.js
@@ -1,15 +1,15 @@
 function getIndexToIns(arr, num) {
 
-  arr.sort(function(a, b) {
+  arr.sort(function(a, b) {         // sort the array from small to big numbers
     return a - b;
   });
 
-  var i = 0;
-  while (num > arr[i]) {
+  var i = 0;                        // define i as 0 to loop through the arr
+  while (num > arr[i]) {            // loop as long as num is bigger then the number i in the array
     i++;
   }
 
-  return i;
+  return i;                         // return the index number where num belongs in arr
 }
 
-getIndexToIns([40, 60], 50);
+getIndexToIns([40, 60], 50);        // i = 2 
